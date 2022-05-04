8e34d1eec78587bbc70eca281189b335648397a2
Mayu Suzumura <47727645+mayupaca@users.noreply.github.com>
2022-05-04 09:15:20 -0400

    Organism

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryBtton";
 import { SecondaryBtton } from "./components/atoms/button/SecondaryBtton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "Jessica",
+  image: "https://source.unsplash.com/NE0XGVKTmcA",
+  email: "111111@sssss.com",
+  phone: "647-123-4569",
+  company: {
+    name: "Website"
+  },
+  website: "https://google.com"
+};
 
 export const App = () => {
   return (
@@ -10,6 +22,7 @@ export const App = () => {
       <SecondaryBtton>Search</SecondaryBtton>
       <br />
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 };
